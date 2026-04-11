00e897c2184c0af67eba8aecf384ff587ef91f2a
Evgeniy Snetilov <evgeniy.snetilov@gmail.com>
2026-04-11 22:12:51 +0300

    Added nvim-tree

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,4 +3,7 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-
+-- Nvim-tree
+keymap.set("n", "<leader>ee", ":NvimTreeToggle<CR>") -- toggle file explorer
+keymap.set("n", "<leader>er", ":NvimTreeFocus<CR>") -- toggle focus to file explorer
+keymap.set("n", "<leader>ef", ":NvimTreeFindFile<CR>") -- find file in file explorer
