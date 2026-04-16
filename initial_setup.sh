4c3b05d03e9b504e651032fb7678335497dfd6fd
Evgeniy Snetilov <evgeniy.snetilov@gmail.com>
2026-04-16 22:17:30 +0300

    Added some additional dependencies

--- initial_setup.sh
+++ initial_setup.sh
@@ -4,4 +4,12 @@
 mkdir -R ~/.local/bin
 curl -sS https://starship.rs/install.sh | sh -s -- --yes --bin-dir ~/.local/bin
 
-apt install fzf fd lua5.1 luarocks neovim openjdk maven gradle
+apt install fzf fd eza node tree-sitter-cli lua5.1 luarocks neovim openjdk maven gradle
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# On Debian
+# cd /usr/local/bin
+# sudo wget https://github.com/neovim/neovim/releases/download/v0.12.1/nvim-linux-x86_64.tar.gz
+# sudo tar zxf nvim-linux-x86_64.tar.gz
+# ln -s nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
