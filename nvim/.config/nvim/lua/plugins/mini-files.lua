c249314c910c50da2f7b5fbed6550dc950cde1e3
Evgeniy Snetilov <evgeniy.snetilov@gmail.com>
2026-04-25 17:13:30 +0300

    Start mini.files if vim opens folder

--- nvim/.config/nvim/lua/plugins/mini-files.lua
+++ nvim/.config/nvim/lua/plugins/mini-files.lua
@@ -225,5 +225,17 @@ return {
         end
       end,
     })
+
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function()
+        local arg = vim.fn.argv(0)
+
+        if arg ~= "" and vim.fn.isdirectory(arg) == 1 then
+          vim.schedule(function()
+            require("mini.files").open(arg)
+          end)
+        end
+      end,
+    })
   end,
 }
